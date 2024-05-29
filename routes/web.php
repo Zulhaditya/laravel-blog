51e9f4200dd9c13dcc4ab4c3d2538ee93139fe85
Zulhaditya <zulhaditya@gmail.com>
2024-05-29 23:12:56 +0700

    Implementasi view data posts dan single post

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,67 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
+
 Route::get('/', function () {
-    return view('home', ['title' => 'Homepage']);
+  return view('home', ['title' => 'Homepage']);
 });
 
 Route::get('/about', function () {
-    return view('about', ['name' => 'Inayah Fitri Wulandari', 'title' => 'About']);
+  return view('about', ['name' => 'Inayah Fitri Wulandari', 'title' => 'About']);
 });
 
-Route::get('/blog', function () {
-    return view('blog', ['title' => 'Blog']);
+Route::get('/posts', function () {
+  return view('posts', ['title' => 'Blog', 'posts' => [
+    [
+      'id' => 1,
+      'slug' => 'judul-artikel-1',
+      'title' => 'Judul Artikel 1',
+      'author' => 'Zulhaditya Hapiz',
+      'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo tempora eius hic. Distinctio
+      eveniet quae rem voluptates itaque veniam asperiores maiores. Laboriosam magnam, cum est odit quos repellat sapiente quod!'
+    ],
+    [
+      'id' => 2,
+      'title' => 'Judul Artikel 2',
+      'slug' => 'judul-artikel-2',
+      'author' => 'Inayah Wulandari',
+      'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti iusto esse, facere,
+      autem officiis assumenda deserunt id quam doloremque totam magni distinctio porro animi ut mollitia! Repellat sed
+      deserunt debitis?'
+    ]
+  ]]);
 });
 
 Route::get('/contact', function () {
-    return view('contact', ['title' => 'Contact']);
+  return view('contact', ['title' => 'Contact']);
+});
+
+Route::get('/posts/{slug}', function ($slug) {
+  $posts = [
+    [
+      'id' => 1,
+      'slug' => 'judul-artikel-1',
+      'title' => 'Judul Artikel 1',
+      'author' => 'Zulhaditya Hapiz',
+      'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo tempora eius hic. Distinctio
+      eveniet quae rem voluptates itaque veniam asperiores maiores. Laboriosam magnam, cum est odit quos repellat sapiente quod!'
+    ],
+    [
+      'id' => 2,
+      'slug' => 'judul-artikel-2',
+      'title' => 'Judul Artikel 2',
+      'author' => 'Inayah Wulandari',
+      'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti iusto esse, facere,
+      autem officiis assumenda deserunt id quam doloremque totam magni distinctio porro animi ut mollitia! Repellat sed
+      deserunt debitis?'
+    ]
+  ];
+
+  $post = Arr::first($posts, function ($post) use ($slug) {
+    return $post['slug'] == $slug;
+  });
+
+  return view('post', ['title' => 'Single Post', 'post' => $post]);
 });
