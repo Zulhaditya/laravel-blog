76107ca5390b48a49b1e46929eccf6497b06ff8b
Zulhaditya <zulhaditya@gmail.com>
2024-05-29 23:34:50 +0700

    Implementasi konsep mvc dan namespace pada blogpost

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
@@ -13,25 +14,7 @@ Route::get('/about', function () {
 });
 
 Route::get('/posts', function () {
-  return view('posts', ['title' => 'Blog', 'posts' => [
-    [
-      'id' => 1,
-      'slug' => 'judul-artikel-1',
-      'title' => 'Judul Artikel 1',
-      'author' => 'Zulhaditya Hapiz',
-      'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo tempora eius hic. Distinctio
-      eveniet quae rem voluptates itaque veniam asperiores maiores. Laboriosam magnam, cum est odit quos repellat sapiente quod!'
-    ],
-    [
-      'id' => 2,
-      'title' => 'Judul Artikel 2',
-      'slug' => 'judul-artikel-2',
-      'author' => 'Inayah Wulandari',
-      'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti iusto esse, facere,
-      autem officiis assumenda deserunt id quam doloremque totam magni distinctio porro animi ut mollitia! Repellat sed
-      deserunt debitis?'
-    ]
-  ]]);
+  return view('posts', ['title' => 'Blog', 'posts' => Post::all()]);
 });
 
 Route::get('/contact', function () {
@@ -39,29 +22,7 @@ Route::get('/contact', function () {
 });
 
 Route::get('/posts/{slug}', function ($slug) {
-  $posts = [
-    [
-      'id' => 1,
-      'slug' => 'judul-artikel-1',
-      'title' => 'Judul Artikel 1',
-      'author' => 'Zulhaditya Hapiz',
-      'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo tempora eius hic. Distinctio
-      eveniet quae rem voluptates itaque veniam asperiores maiores. Laboriosam magnam, cum est odit quos repellat sapiente quod!'
-    ],
-    [
-      'id' => 2,
-      'slug' => 'judul-artikel-2',
-      'title' => 'Judul Artikel 2',
-      'author' => 'Inayah Wulandari',
-      'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti iusto esse, facere,
-      autem officiis assumenda deserunt id quam doloremque totam magni distinctio porro animi ut mollitia! Repellat sed
-      deserunt debitis?'
-    ]
-  ];
-
-  $post = Arr::first($posts, function ($post) use ($slug) {
-    return $post['slug'] == $slug;
-  });
+  $post = Post::find($slug);
 
   return view('post', ['title' => 'Single Post', 'post' => $post]);
 });
