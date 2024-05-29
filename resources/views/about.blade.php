bb628b4163d4d676ef8ec958a53911b1b3242730
Zulhaditya <zulhaditya@gmail.com>
2024-05-29 15:04:55 +0700

    Implementasi blade component

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,22 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>About</title>
-    <link rel="stylesheet" href="css/style.css">
-</head>
-
-<body>
-    <a href="/">Home</a>
-    <a href="/about">About</a>
-    <a href="/blog">Blog</a>
-    <a href="/contact">Contact</a>
-
-    <h1>About Page</h1>
-    <h3>Owner: {{ $name; }}</h3>
-    <img src="img/girl-white.jpg" alt="girl-white" width="200" height="200">
-</body>
-
-</html>
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot:title>
+    <h3 class="text-xl">About page</h3>
+    <p>Nama: {{ $name }}</p>
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -3,26 +3,17 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', ['title' => 'Homepage']);
 });
 
 Route::get('/about', function () {
-    return view('about', ['name' => 'Inayah']);
+    return view('about', ['name' => 'Inayah Fitri Wulandari', 'title' => 'About']);
 });
 
 Route::get('/blog', function () {
-    return view(
-        'blog',
-        ['title' => 'article-1', 'author' => 'author-1', 'body' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae asperiores at, facere aliquid quam minus aut iusto porro quia enim aliquam delectus a voluptatum consequatur veritatis labore reprehenderit maxime esse!'],
-        ['title' => 'article-2', 'author' => 'author-2', 'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nam expedita voluptatum cumque odio labore voluptatem eveniet tempore vero consequuntur. Molestias quia asperiores ratione, fugit reprehenderit voluptatem! Eveniet accusamus neque saepe?
-        '],
-    );
+    return view('blog', ['title' => 'Blog']);
 });
 
 Route::get('/contact', function () {
-    return view(
-        'contact',
-        ['instagram' => 'https://instagram.com/zulhaditya'],
-        ['github' => 'https://github.com/zulhaditya'],
-    );
+    return view('contact', ['title' => 'Contact']);
 });
