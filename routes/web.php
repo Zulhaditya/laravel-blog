c8952c38635a4971ba3bcafd9eda8bb0decb6358
Zulhaditya <zulhaditya@gmail.com>
2024-06-02 22:16:01 +0700

    Implementasi eloquent relationship

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\Post;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 
@@ -22,4 +23,8 @@ Route::get('/contact', function () {
 
 Route::get('/posts/{post:slug}', function (Post $post) {
   return view('post', ['title' => 'Single Post', 'post' => $post]);
+});
+
+Route::get('/authors/{user}', function (User $user) {
+  return view('posts', ['title' => 'Articles by ' . $user->name, 'posts' => $user->posts]);
 });
